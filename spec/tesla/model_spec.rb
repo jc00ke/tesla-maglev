4d1644bddece7a549f6545f03c1b281e4f751bd7
jc00ke <jesse@jc00ke.com>
2011-02-01 12:54:10 -0800

    Add basic spec for persistability on class

--- spec/tesla/model_spec.rb
+++ spec/tesla/model_spec.rb
@@ -1,7 +1,14 @@
 require File.join(File.dirname(__FILE__), '..', 'spec_helper')
 
 describe Tesla::Model do
-  it "should fail" do
-    flunk "fail!"
+
+  describe "persistably included" do
+    before do
+      class ::Egg; include Tesla::Model end
+    end
+
+    it "should pass along persistability" do
+      ::Egg.maglev_persistable?.must_equal true
+    end
   end
 end
