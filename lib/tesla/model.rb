1c7a71c3b46d695796669de5fd58bebd211caebb
jc00ke <jesse@jc00ke.com>
2011-02-01 15:09:54 -0800

    Committing class & instance methods
    Mostly a pull from ecin's persistable but cleaned up a little
    to use ActiveSupport::Concern

--- lib/tesla/model.rb
+++ lib/tesla/model.rb
@@ -10,13 +10,66 @@ module Tesla
 
     # methods that will be available to the included class, eg: User.foo
     module ClassMethods
+      include Enumerable
+      extend Forwardable
+
+      # The Identity Set that keeps all the persisted instances
+      def store
+        @store ||= begin
+                     raise "This is Maglev only" unless defined?(Maglev)
+                     # In the future, it might be nice to use something
+                     # other than an IdentitySet. Set? Bag?
+                     Maglev::PERSISTENT_ROOT[self] ||= IdentitySet.new
+                   end
+      end
+
+      # Give access to SOME of @store's instance methods.
+      # May want to expand this list at some point.
+      # Most of the remaining useful ones are shared with Array, so they can be used
+      # after calling #to_a first.
+      def_delegators :store, :classify, :clear, :delete, :delete?, :delete_if, :each, :empty?, :size, :to_a
+
+      # There's no default sorting, as IdentitySet doesn't have it.
+      # Maybe add in later?
+      alias all to_a
+
+      # If the arg is a module, check manually. Otherwise, we're probably talking
+      # about #include? on the store.
+      def include?(arg)
+        arg.is_a?(Module) ? !!included_modules.detect{ |m| m === arg } : store.include?(arg)
+      end
+
+      # Change this after Peter adds IdentitySet#clear
+      # https://gist.github.com/806923
+      def delete_all
+        all.each{ |o| delete(o) }
+      end
       
     end
 
     # methods that will be available to instances of a class, eg: User.new.bar
     module InstanceMethods
+      
+      # Store the instance in the store
+      def persist
+        !!self.class.store.add?(self)
+      end
+
+      # Remove the instance from the store
+      def desist
+        self.class.delete self
+      end
+
+      # Already persisted?
+      def persistent?
+        self.class.include? self
+      end
+
+      # Yet to be persisted?
+      def transient?
+        !persistent?
+      end
 
     end
-
   end
 end
--- spec/tesla/model_spec.rb
+++ spec/tesla/model_spec.rb
@@ -1,14 +1,85 @@
 require File.join(File.dirname(__FILE__), '..', 'spec_helper')
 
+class ::Egg; include Tesla::Model end
+class ::Bacon < Struct.new(:type); include Tesla::Model end
+
 describe Tesla::Model do
 
   describe "persistably included" do
+    it "should pass along persistability" do
+      assert ::Egg.maglev_persistable?
+    end
+  end
+
+  describe "instances" do
     before do
-      class ::Egg; include Tesla::Model end
+      @soggy    = Bacon.new "soggy"
+      @crunchy  = Bacon.new "crunchy"
+      @chunky   = Bacon.new "chunky"
+      @cloggers = [@soggy, @crunchy, @chunky]
     end
 
-    it "should pass along persistability" do
-      ::Egg.maglev_persistable?.must_equal true
+    after do
+      Bacon.delete_all
     end
+
+    def persist_all
+      @cloggers.each{ |bacon| bacon.persist }
+    end
+
+    it "should start off empty" do
+      assert Bacon.all.empty?
+    end
+
+    it "should persist instances" do
+      persist_all
+      breakfast = Bacon.all
+      Bacon.all.count.must_equal 3
+      assert @cloggers.all?{ |bacon| breakfast.include?(bacon) }
+    end
+
+    describe "count" do
+      it "should return number of persistent instances" do
+        @cloggers.each_with_index do |bacon, i|
+          bacon.persist
+          Bacon.count.must_equal(i + 1)
+        end
+      end
+    end
+
+    describe "delete_all" do
+      it "should, um, remove all instances" do
+        persist_all
+        Bacon.delete_all
+        Bacon.count.must_equal 0
+      end
+    end
+
+    it "should support some querying" do
+      persist_all
+      @cloggers.each do |bacon|
+        results = Bacon.select{ |b| b.type[/#{bacon.type}/] }
+        results.size.must_equal 1
+        assert results.include?(bacon)
+      end
+    end
+
+    it "should know if it has been persistent" do
+      assert @chunky.transient?
+      @chunky.persist
+      assert @chunky.persistent?
+    end
+
+    it "should be desisted" do
+      @chunky.desist
+      assert @chunky.transient?
+    end
+
+    it "should keep include? & not forward it on" do
+      @soggy.persist
+      assert Bacon.include?(@soggy), "enumberable include? should work"
+      assert Bacon.include?(Tesla::Model), "testing module include? should work"
+    end
+    
   end
 end
